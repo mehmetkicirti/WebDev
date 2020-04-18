cdfab7d39ac4a31715f00f57cc0560bb8b891a09
mehmet <mehmetkicirti@hotmail.com>
2020-04-18 23:43:21 +0300

    Erişmek istediğimiz kullanıcak oldugumuz nesneleri oluşturduk sadece Book için

---
+++ Entities/Concrete/Book.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    //EF sınıfına gönderebilecegim anlamına geliyor IEntity ile
+    public class Book:IEntity
+    {
+        public int Id { get; set; }
+        public int AuthorId { get; set; }
+        public string Name{ get; set; }
+        public decimal Price { get; set; }
+    }
+}
