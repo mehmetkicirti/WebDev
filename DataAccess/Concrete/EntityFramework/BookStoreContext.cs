bc6341c9051f1aab9e6daac7bb7ecac84feffa55
mehmet <mehmetkicirti@hotmail.com>
2020-04-19 00:13:18 +0300

    Changes Db ConnectionString file

--- DataAccess/Concrete/EntityFramework/BookStoreContext.cs
+++ DataAccess/Concrete/EntityFramework/BookStoreContext.cs
@@ -11,8 +11,10 @@ namespace DataAccess.Concrete.EntityFramework
         //Tablondaki books isimli tablona denk gelir diyoruz.
         public DbSet<Book> Books{ get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\ProjectsV13;Database=BookStore;Trusted_Connection=true;");
+            //Data Source=(localdb)\ProjectsV13;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\ProjectsV13;Initial Catalog=BookStore;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
         }
     }
 }
