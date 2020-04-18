77e512d6509bd6c08fcdaec72a97c7df68404721
mehmet <mehmetkicirti@hotmail.com>
2020-04-18 23:44:39 +0300

    İş kodlarımızı tanımladık BookSınıfı için

---
+++ Business/Abstract/IBookService.cs
@@ -0,0 +1,25 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    //İş Kodu works Here
+    //Örneğin bir adam kredi alıcak uygunmu
+    //Dataaccess ile iletiişim kurar.
+    //İş kuralları değişken olduğu için sadece dataAccess de kullanılır ortak bir Erişim katmanı generic yapı olusturmak core dataaccess deki
+    public interface IBookService
+    {
+        //Ortak bir interface Business Katmanında olmaz değişkenlik gösterebilir her bir servise göre
+        //burada artık yapacagımız işleri tanımlıyoruz
+        List<Book> GetAll();
+        Book GetById(int id);
+        List<Book> GetByAuthorId(int authorId);
+        
+        //List<Book> GetByIsbn(string Isbn);
+        void Add(Book entity);
+        void Delete(Book entity);
+        void Update(Book entity);
+    }
+}
---
+++ Business/Concrete/BookManager.cs
@@ -0,0 +1,56 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Business.Concrete
+{
+    //Kitapla ilgili servisleri burada yönetiyoruz artık
+    public class BookManager : IBookService
+    {
+        //IbookdAL field vermemizin yalnızca sebebi erişebililelim diye
+        //interfaceler referans tiptir bu yüzden hangi referansı verirsem onu kullanarak
+        private IBookDal _bookDal;
+        public BookManager(IBookDal bookDal)
+        {
+            _bookDal = bookDal;
+        }
+        public void Add(Book entity)
+        {
+            //iş kodu
+            //Örnegin bu iş kuralını biz birden fazla yerde kullanabiliriz bu yüzden bu kod kötü bunu ayrı bir yerde ele almamız lazım.
+            if(_bookDal.Get(b=>b.Name == entity.Name && b.AuthorId == entity.AuthorId) == null)
+            {
+                _bookDal.Add(entity);
+            }else throw new Exception("Bu kitap daha önce eklenmiş");
+        }
+
+        public void Delete(Book entity)
+        {
+            _bookDal.Delete(entity);
+        }
+
+        public List<Book> GetAll()
+        {
+            return _bookDal.GetList().ToList();   
+        }
+
+        public List<Book> GetByAuthorId(int authorId)
+        {
+            return _bookDal.GetList(b=> b.AuthorId == authorId).ToList();
+        }
+
+        public Book GetById(int id)
+        {
+            return _bookDal.Get(b=>b.Id == id);
+        }
+
+        public void Update(Book entity)
+        {
+            _bookDal.Update(entity);
+        }
+    }
+}
